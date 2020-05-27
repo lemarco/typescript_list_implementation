e96794c3bd1f54aab69739a6a1d6557902f296f1
lemarco <igor.hoienko@refinitiv.com>
2020-05-27 23:00:53 +0300

    linter fix

--- src/List.ts
+++ src/List.ts
@@ -7,16 +7,12 @@ interface Node<T> {
 }
 
 export default class List<T> {
-      private node: Node<T> = {
+    private node: Node<T> = {
         next: null,
         prev: null,
-        value:null
-      };
-      private length : number = 0; 
-      private head : Node<T> = null;
-      private tail : Node<T> = null;
-
-      
-
-    }
+        value: null,
+    };
+    private length = 0;
+    private head: Node<T> = null;
+    private tail: Node<T> = null;
 }
