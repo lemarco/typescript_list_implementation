3c89c654204ce0f558cabfe6782aa515c075cdc1
lemarco <igor.hoienko@refinitiv.com>
2020-05-27 23:18:52 +0300

    signatures of methods

--- src/List.ts
+++ src/List.ts
@@ -12,7 +12,26 @@ export default class List<T> {
         prev: null,
         value: null,
     };
-    private length = 0;
+    private _length = 0;
     private head: Node<T> = null;
     private tail: Node<T> = null;
+
+    private createNode<T>({ value, next = null, prev = null }: { value: T; next?: Node<T>; prev?: Node<T> }): Node<T> {
+        return {
+            next,
+            prev,
+            value,
+        };
+    }
+    public push(node: Node<T>) {}
+    public pop(node: Node<T>) {}
+    public shift(node: Node<T>) {}
+    public unShift(node: Node<T>) {}
+    public sort(): void {}
+    public reverse(): void {}
+    // public toArray():[<T>]{}
+    public forEach(): void {}
+    get length(): number {
+        return this._length;
+    }
 }
