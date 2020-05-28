6dfafed3826f43f33ec7747275d43b051186a7b5
lemarco <igor.hoienko@refinitiv.com>
2020-05-28 08:50:09 +0300

    push method create node fix

--- src/List.ts
+++ src/List.ts
@@ -7,23 +7,49 @@ interface Node<T> {
 }
 
 export default class List<T> {
-    private node: Node<T> = {
-        next: null,
-        prev: null,
-        value: null,
-    };
-    private _length = 0;
-    private head: Node<T> = null;
-    private tail: Node<T> = null;
+  private _length = 0;
+  private head: Node<T> = {
+      next: null,
+      prev: null,
+      value: null,
+  };
+    private tail: Node<T> = {
+      next: null,
+      prev: null,
+      value: null,
+  };
+    private iterator:Node<T> = null
 
-    private createNode<T>({ value, next = null, prev = null }: { value: T; next?: Node<T>; prev?: Node<T> }): Node<T> {
+    private createNode( value:T, next?: Node<T>, prev?: Node<T> ): Node<T> {
         return {
             next,
             prev,
             value,
         };
     }
-    public push(node: Node<T>) {}
+    public push(value:any) {
+      if (!this._length){
+        this.head = this.createNode(value)
+        this.tail = this.head
+        this._length++;
+      }
+      const node = this.createNode(value, null, this.tail);
+      this.addNextNode(this.tail, node)
+      this.tail = node
+      this._length++
+
+    }
+    private addNextNode(current:Node<T>, next:Node<T>):void{
+      if (!current.next){
+        current.next = next
+        next.prev = current
+      }
+      const temp = current.next
+      current.next = next
+      next.prev = current
+      next.next = temp
+      temp.prev = next
+    }
     public pop(node: Node<T>) {}
     public shift(node: Node<T>) {}
     public unShift(node: Node<T>) {}
