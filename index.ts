fa4025d5ddca10a702861c523147560487b1ab89
lemarco <igor.hoienko@refinitiv.com>
2020-05-28 19:32:45 +0300

    testing push pop shift unshift

--- index.ts
+++ index.ts
@@ -1,4 +1,23 @@
 
 import List from './src/List'
 
-const list = new List
+const list = new  List<string>()
+
+console.log("___list.push()___")
+list.push("masha")
+list.showList()
+console.log("___list.push()___")
+list.push("baklaha")
+list.showList()
+console.log("___list.push()___")
+list.push("gogo")
+list.showList()
+console.log("___list.push()___")
+list.push("yyyy")
+list.showList()
+console.log("___list.pop()___")
+list.pop()
+list.showList()
+console.log("___list.shift()___")
+list.shift()
+list.showList()
--- src/List.ts
+++ src/List.ts
@@ -1,5 +1,3 @@
-import color from 'color';
-
 interface Node<T> {
     next: Node<T> | null;
     prev: Node<T> | null;
@@ -7,6 +5,106 @@ interface Node<T> {
 }
 
 export default class List<T> {
+    
+    public push(value: T) {
+        if (!this._length) {
+            const newNode = this.createNode(value);
+            this.head = newNode;
+            this.tail = newNode;
+            this._length++;
+            return;
+        }
+        const node = this.createNode(value, null, this.tail);
+        this.addNextNode(this.tail, node);
+        this.tail = node;
+        this._length++;
+    }
+    public pop(): Node<T> | null {
+        if (!this._length) return null;
+        const newTail = this.tail.prev;
+        const pop = this.tail;
+        newTail.next = null;
+        this.tail = newTail;
+        pop.prev = null;
+        //this.showNode(pop)
+        this._length--;
+        return pop;
+    }
+    public shift(): Node<T> | null {
+        if (!this._length) return null;
+        const node = this.head 
+        this.head = this.head.next
+        this.head.prev = null
+        this._length--;
+        node.next = null
+        return node
+    }
+    public unShift(value: T) {
+        if (!this._length) {
+            this.head = this.createNode(value);
+            this.tail = this.head;
+            this._length++;
+            return;
+        }
+        const node = this.createNode(value);
+        this.addNextNode(node, this.head);
+        this.head = node;
+    }
+    public sort(): void {}
+
+    public reverse(): void {
+      let leftIterator: Node<T> = this.head 
+      let rightIterator : Node<T> = this.tail
+
+      for (let i = 0 ; i < this._length / 2; i++) {
+      }
+    }
+    public toArray(): T[] {
+        const array: T[] = [];
+        this.forEach((node: Node<T>) => {
+            array.push(node.value);
+        });
+        return array;
+    }
+    public forEach(f: (node: Node<T>) => void): void {
+        this.iterator = this.head;
+        while (this.iterator.next) {
+            f(this.iterator);
+            this.iterator = this.iterator.next;
+        }
+    }
+    get length(): number {
+        return this._length;
+    }
+    public showNode(node: Node<T>) {
+        process.stdout.write('{ ');
+        process.stdout.write('node.value = ' + `${node.value} `);
+        process.stdout.write(' *** ');
+        if (node.next) {
+            if (node.next.value) {
+                process.stdout.write('node.next.value = ' + `${node.next.value} `);
+            }
+        } else {
+            process.stdout.write('node.next.value : ' + 'null ');
+        }
+        process.stdout.write(' *** ');
+
+        if (node.prev) {
+            if (node.prev.value) process.stdout.write('node.prev.value = ' + `${node.prev.value} `);
+        } else {
+            process.stdout.write('node.prev.value : ' + 'null ');
+        }
+        process.stdout.write('  } ---> \n');
+    }
+    public showList() {
+        let iterator = this.head;
+        this.showNode(iterator);
+        while (iterator.next) {
+            this.showNode(iterator.next);
+            iterator = iterator.next;
+        }
+        console.log('NULL');
+    }
     private _length = 0;
     private head: Node<T> = {
         next: null,
@@ -27,72 +125,16 @@ export default class List<T> {
             value,
         };
     }
-    public push(value: T) {
-        if (!this._length) {
-            this.head = this.createNode(value);
-            this.tail = this.head;
-            this._length++;
-        }
-        const node = this.createNode(value, null, this.tail);
-        this.addNextNode(this.tail, node);
-        this.tail = node;
-        this._length++;
-    }
     private addNextNode(current: Node<T>, next: Node<T>): void {
-        if (!current.next) {
-            current.next = next;
-            next.prev = current;
-        }
-        const temp = current.next;
-        current.next = next;
-        next.prev = current;
-        next.next = temp;
-        temp.prev = next;
-    }
-    public pop(node: Node<T>): Node<T> | null {
-        if (!this._length) return null;
-        const newTail = this.tail.prev;
-        const popNode = this.tail;
-        newTail.next = null;
-        this.tail = newTail;
-        popNode.prev = null;
-        return popNode;
-    }
-    public shift(node: Node<T>) {
-        if (!this._length) return null;
-        const shiftNode = this.head;
-        const newHead = shiftNode.next;
-
-        shiftNode.next = null;
-        newHead.prev = null;
-        this.head = newHead;
-        return shiftNode;
-    }
-    public unShift(value: T) {
-        if (!this._length) {
-            this.head = this.createNode(value);
-            this.tail = this.head;
-            this._length++;
-        }
-        const node = this.createNode(value);
-        this.addNextNode(node, this.head);
-        this.head = node;
-    }
-    public sort(): void {}
-    public reverse(): void {}
-    public toArray():T[]{
-      const array: T[] = []
-      this.forEach((node:Node<T>)=>{array.push(node.value)})
-      return array;
-    }
-    public forEach(f:(node: Node<T>) => void): void {
-      this.iterator = this.head
-      while(this.iterator.next){
-        f(this.iterator)
-        this.iterator = this.iterator.next
+      if (!current.next) {
+          current.next = next;
+          next.prev = current;
+          return;
       }
-    }
-    get length(): number {
-        return this._length;
-    }
+      const temp = current.next;
+      current.next = next;
+      next.prev = current;
+      next.next = temp;
+      temp.prev = next;
+  }
 }
